b557f8dd21bf64072d97a30b853c4c4a306cf43c
svenroegiers <sven.roegiers@student.howest.be>
2015-08-15 12:48:00 +0200

    openshiftmongo

--- app.js
+++ app.js
@@ -16,8 +16,18 @@ var topics = {};
 var dbConfig = require('./db');
 var mongoose = require('mongoose');
 
+var connection_string = '127.0.0.1:27017/ratingapp';
+
+if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+    process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+    process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+    process.env.OPENSHIFT_APP_NAME;
+}
+
 // Connect to DB
-mongoose.connect(dbConfig.url);
+mongoose.connect('mongodb://' + connection_string);
 
 var app = express();
 
